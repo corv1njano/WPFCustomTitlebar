a7571db657bf891599dd362ed0e146d5083a4cb5
corv1njano <corvin.gundacker@gmx.de>
2026-01-29 00:28:29 +0100

    Upload

---
+++ CustomTitlebar1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using CustomTitlebar1.Utilities;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomTitlebar1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly WindowWrapper _wrapper;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _wrapper = new WindowWrapper(this);
+
+            StateChanged += MainWindow_StateChanged;
+        }
+
+        private void MainWindow_StateChanged(object? sender, EventArgs e)
+        {
+            MaximizeRestoreIcon.ImageSource = WindowState switch
+            {
+                WindowState.Maximized => (ImageSource)FindResource("WindowControl.Restore"),
+                WindowState.Normal => (ImageSource)FindResource("WindowControl.Maximize"),
+                _ => MaximizeRestoreIcon.ImageSource
+            };
+        }
+
+        private void Button_Minimize(object sender, RoutedEventArgs e)
+        {
+            _wrapper.MinimizeWindow();
+        }
+
+        private void Button_MaximizeRestore(object sender, RoutedEventArgs e)
+        {
+            _wrapper.MaximizeOrRestoreWindow();
+        }
+
+        private void Button_Close(object sender, RoutedEventArgs e)
+        {
+            _wrapper.CloseWindow();
+        }
+    }
+}
---
+++ CustomTitlebar1/Utilities/WindowWrapper.cs
@@ -0,0 +1,70 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomTitlebar1.Utilities
+{
+    public class WindowWrapper
+    {
+        private readonly Window _targetWindow;
+        private readonly Thickness _baseBorder;
+
+        private const int SM_CXPADDEDBORDER = 92;
+        [DllImport("user32.dll")]
+        private static extern int GetSystemMetrics(int nIndex);
+
+        public WindowWrapper(Window targetWindow)
+        {
+            _targetWindow = targetWindow;
+            _baseBorder = _targetWindow.BorderThickness;
+
+            _targetWindow.StateChanged += OnStateChanged;
+        }
+
+        public void CloseWindow()
+        {
+            _targetWindow.Close();
+        }
+
+        public void MinimizeWindow()
+        {
+            _targetWindow.WindowState = WindowState.Minimized;
+        }
+
+        public void MaximizeOrRestoreWindow()
+        {
+            _targetWindow.WindowState = _targetWindow.WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
+
+        private void OnStateChanged(object? sender, EventArgs e)
+        {
+            Thickness sysResize = SystemParameters.WindowResizeBorderThickness;
+
+            double padded = GetPaddedBorder(_targetWindow);
+
+            if (_targetWindow.WindowState == WindowState.Maximized)
+            {
+                _targetWindow.BorderThickness = new Thickness(
+                    _baseBorder.Left + sysResize.Left + padded,
+                    _baseBorder.Top + sysResize.Top + padded,
+                    _baseBorder.Right + sysResize.Right + padded,
+                    _baseBorder.Bottom + sysResize.Bottom + padded
+                );
+            }
+            else
+            {
+                _targetWindow.BorderThickness = _baseBorder;
+            }
+        }
+
+        private static double GetPaddedBorder(Visual visual)
+        {
+            var dpi = VisualTreeHelper.GetDpi(visual);
+            int paddedPx = GetSystemMetrics(SM_CXPADDEDBORDER);
+
+            return paddedPx / dpi.DpiScaleX;
+        }
+    }
+}
